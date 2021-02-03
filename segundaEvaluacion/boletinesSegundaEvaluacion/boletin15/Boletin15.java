fb947f0e861c9935ac54f82fae4c31574dd19339
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2021-02-03 21:53:30 +0100

    Boletín 15 en proceso.

---
+++ segundaEvaluacion/boletinesSegundaEvaluacion/boletin15/Boletin15.java
@@ -0,0 +1,17 @@
+package boletin15;
+import libreriaAngel.*;
+
+/**
+ * Creado por @autor: angel
+ * El  03 de feb. de 2021.
+ **/
+public class Boletin15 {
+    public static void main(String[] args) {
+    Arrays.crearArrayAleatorio();
+    Arrays.verArrayAleatorio(Arrays.crearArrayAleatorio());
+
+
+
+
+    }
+}
