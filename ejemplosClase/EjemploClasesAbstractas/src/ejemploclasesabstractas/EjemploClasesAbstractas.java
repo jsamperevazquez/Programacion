cf202adf861f81fe0747dddabbc1e9cf5c27a7dc
Jose Ángel <jsamperevazquez@danielcastelao.org>
2021-04-16 12:06:44 +0200

    Instanciados objetos

--- ejemplosClase/EjemploClasesAbstractas/src/ejemploclasesabstractas/EjemploClasesAbstractas.java
+++ ejemplosClase/EjemploClasesAbstractas/src/ejemploclasesabstractas/EjemploClasesAbstractas.java
@@ -12,7 +12,11 @@ package ejemploclasesabstractas;
 public class EjemploClasesAbstractas {
 
     public static void main(String[] args) {
-        
+        Rectangulo rectangulo1= new Rectangulo(4, 5);
+        Triangulo triangulo1= new Triangulo(5, 4);
+        System.out.println(rectangulo1.calcularArea());
+        System.out.println(triangulo1.calcularArea());
+        System.out.println(rectangulo1.calcularPerimetro());
     }
     
 }
