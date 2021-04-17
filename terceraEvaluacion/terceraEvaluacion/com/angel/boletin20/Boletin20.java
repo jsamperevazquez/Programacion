7cfdba84e5bb534197a1fe819f0492b18aa30d30
Jose Ángel <jsamperevazquez@danielcastelao.org>
2021-04-17 17:59:02 +0200

    Boletín 20 terminado (Sin funcionalidad)

---
+++ terceraEvaluacion/terceraEvaluacion/com/angel/boletin20/Boletin20.java
@@ -0,0 +1,14 @@
+package com.angel.boletin20;
+
+/**
+ * Creado por @autor: angel
+ * El  17 de abr. de 2021.
+ * //-encoding utf8 -docencoding utf8 -charset utf8(Para el javadoc)
+ **/
+public class Boletin20 {
+    public static void main(String[] args) {
+        Componentes componentes = new Componentes();
+        componentes.iniciar();
+
+    }
+}
