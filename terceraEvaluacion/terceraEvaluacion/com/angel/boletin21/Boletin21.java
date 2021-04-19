0cc01cb9b5b9883c5a896bc3c2e57290c57682f8
Jose Ángel <jsamperevazquez@danielcastelao.org>
2021-04-19 12:13:32 +0200

    Boletin 21 parte 1 terminado con Jgoonies

---
+++ terceraEvaluacion/terceraEvaluacion/com/angel/boletin21/Boletin21.java
@@ -0,0 +1,40 @@
+package com.angel.boletin21;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+
+/**
+ * Creado por @autor: angel
+ * El  19 de abr. de 2021.
+ * //-encoding utf8 -docencoding utf8 -charset utf8(Para el javadoc)
+ **/
+public class Boletin21 extends JFrame {
+
+
+    private JPanel panel;
+    private JLabel userNameLabel;
+    private JTextField nueUserText;
+    private JTextField fullNameText;
+    private JTextField textField3;
+    private JComboBox comboBox1;
+    private JTextField textField4;
+    private JComboBox comboBox2;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JButton OKButton;
+    private JButton cancelButton;
+    private JLabel createNueUser;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Boletin21");
+        frame.setContentPane(new Boletin21().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+
+
+    }
+
+}
