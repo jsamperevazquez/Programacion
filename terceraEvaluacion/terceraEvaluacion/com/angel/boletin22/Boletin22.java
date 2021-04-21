7555777be63dfa4b443f9f2562c88a0b11b34342
Jose Ángel <jsamperevazquez@danielcastelao.org>
2021-04-21 04:10:15 +0200

    Boletín 22 Solucionado error de visualización de la tabla

--- terceraEvaluacion/terceraEvaluacion/com/angel/boletin22/Boletin22.java
+++ terceraEvaluacion/terceraEvaluacion/com/angel/boletin22/Boletin22.java
@@ -1,10 +1,14 @@
 package com.angel.boletin22;
 
+import javafx.scene.control.TableRow;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.JTable;
+import javax.swing.table.TableColumn;
+import javax.swing.text.TableView;
 import java.awt.*;
 
 /**
@@ -12,7 +16,7 @@ import java.awt.*;
  * El  20 de abr. de 2021.
  * //-encoding utf8 -docencoding utf8 -charset utf8(Para el javadoc)
  **/
-public class Boletin22 extends JFrame{
+public class Boletin22 extends JFrame {
     private JPanel panel;
     private JTextField textField1;
     private JComboBox comboAsig;
@@ -24,23 +28,23 @@ public class Boletin22 extends JFrame{
 
     private void createUIComponents() {
         TableCellRenderer renderizado;
-        Object [] titulos={"Nombre","Apellido","Curso"};
-        Object [][] celdas= new Object[2][titulos.length];
-        JScrollPane scrollPane= new JScrollPane();
-        tablaCurso=new JTable(celdas,titulos);
+        Object[] titulos = {"Nombre", "Apellido", "Curso"}; // Creo un array de tipo Object para no limitar con los diferentes nombres para las celdas
+        Object comodin="";
+        Object[][] celdas = new Object[2][titulos.length]; // Creo un array bidimensional con las celdas (filas y columnas) de la tabla
+        JScrollPane scrollPane = new JScrollPane(); // Creo un JScrollPane para poder agregar el Jtable al form y poder usar desplazamiento vertical y horizontal
+        tablaCurso = new JTable(celdas, titulos); // instancio la tabla con las filas y columnas que hemos creado con anterioridad.
         tablaCurso.setVisible(true);
-        scrollPane.setViewportView(tablaCurso);
-        scrollPane.setBounds(20,20,200,100);
+        scrollPane.setViewportView(tablaCurso); // meto la tabla dentro del contenedor
+        scrollPane.setBounds(20, 20, 200, 100);
         getContentPane().add(scrollPane);
-        DefaultTableCellRenderer tcr= new DefaultTableCellRenderer();
-        tcr.setHorizontalAlignment(SwingConstants.CENTER);
-        celdas[0][0]=titulos[0];
-        celdas[0][1]=titulos[1];
-        celdas[0][2]=titulos[2];
-        tablaCurso.getColumnModel().getColumn(0).setCellRenderer(tcr);
-        tablaCurso.getColumnModel().getColumn(1).setCellRenderer(tcr);
-        tablaCurso.getColumnModel().getColumn(2).setCellRenderer(tcr);
+        celdas[0][0] = titulos[0];
+        celdas[0][1] = titulos[1];
+        celdas[0][2] = titulos[2];
+        celdas[1][0] = comodin; // Uso el comodín para que no me de nullpointer en el método de MiRender
+        celdas[1][1] = comodin;
+        celdas[1][2] = comodin;
 
+        tablaCurso.setDefaultRenderer(Object.class, new MiRenderer()); // asocio tabla a clase incrustada (MiRenderer)
 
     }
 
@@ -50,5 +54,28 @@ public class Boletin22 extends JFrame{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    class MiRenderer extends DefaultTableCellRenderer {
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value,
+                                                       boolean isSelected,
+                                                       boolean hasFocus,
+                                                       int row,
+                                                       int column) {
+            String nombreCelda = (String) table.getValueAt(row, 0); // Casteo a String y obtengo valores de la tabla para poder comparar
+
+            if (nombreCelda.equalsIgnoreCase("nombre")) {
+                setBackground(Color.LIGHT_GRAY);
+                setForeground(Color.BLACK);
+                setHorizontalAlignment(SwingConstants.CENTER);
+            }else if (!nombreCelda.equalsIgnoreCase("nombre"))
+                setBackground(Color.WHITE);
+            setForeground(Color.BLACK);
+            setHorizontalAlignment(SwingConstants.CENTER);
+            return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        }
+
     }
 }
