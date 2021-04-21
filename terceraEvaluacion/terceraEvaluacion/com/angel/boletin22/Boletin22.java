239f96a2552002f7875c3922cd242ef5618552d0
Jose Ángel <jsamperevazquez@danielcastelao.org>
2021-04-21 14:30:51 +0200

    Boletín 22 Terminado

--- terceraEvaluacion/terceraEvaluacion/com/angel/boletin22/Boletin22.java
+++ terceraEvaluacion/terceraEvaluacion/com/angel/boletin22/Boletin22.java
@@ -1,15 +1,13 @@
 package com.angel.boletin22;
 
-import javafx.scene.control.TableRow;
-
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.JTable;
-import javax.swing.table.TableColumn;
-import javax.swing.text.TableView;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Creado por @autor: angel
@@ -18,34 +16,42 @@ import java.awt.*;
  **/
 public class Boletin22 extends JFrame {
     private JPanel panel;
-    private JTextField textField1;
+    private JTextField nombreField;
     private JComboBox comboAsig;
     private JLabel nombreLabel;
-    private JTextField textField2;
+    private JTextField apellidoField;
     private JLabel apellidoLabel;
-    private JButton AGREGARTABLAButton;
+    private JButton agregarTablaButton;
     private JTable tablaCurso;
-
+    private DefaultTableModel modelo;
     private void createUIComponents() {
-        TableCellRenderer renderizado;
-        Object[] titulos = {"Nombre", "Apellido", "Curso"}; // Creo un array de tipo Object para no limitar con los diferentes nombres para las celdas
-        Object comodin="";
-        Object[][] celdas = new Object[2][titulos.length]; // Creo un array bidimensional con las celdas (filas y columnas) de la tabla
         JScrollPane scrollPane = new JScrollPane(); // Creo un JScrollPane para poder agregar el Jtable al form y poder usar desplazamiento vertical y horizontal
-        tablaCurso = new JTable(celdas, titulos); // instancio la tabla con las filas y columnas que hemos creado con anterioridad.
+        tablaCurso = new JTable(); // instancio la tabla con las filas y columnas que hemos creado con anterioridad.
         tablaCurso.setVisible(true);
+        // Template para la tabla
+        modelo=new DefaultTableModel(); // Creo un nuevo template para diseñar la tabla
+        // Añado las columnas
+        modelo.addColumn("columnaNombre");
+        modelo.addColumn("columnaApellido");
+        modelo.addColumn("columnaCurso");
+        modelo.addRow(new Object[]{"Nombre","Apellido","Curso"}); // Añado la fila con los valores del array Object
+
+        tablaCurso.setModel(modelo); // Asigno el template a la JTable
         scrollPane.setViewportView(tablaCurso); // meto la tabla dentro del contenedor
         scrollPane.setBounds(20, 20, 200, 100);
         getContentPane().add(scrollPane);
-        celdas[0][0] = titulos[0];
-        celdas[0][1] = titulos[1];
-        celdas[0][2] = titulos[2];
-        celdas[1][0] = comodin; // Uso el comodín para que no me de nullpointer en el método de MiRender
-        celdas[1][1] = comodin;
-        celdas[1][2] = comodin;
 
         tablaCurso.setDefaultRenderer(Object.class, new MiRenderer()); // asocio tabla a clase incrustada (MiRenderer)
 
+    }
+
+    public Boletin22() {
+        agregarTablaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                modelo.addRow(new Object[]{nombreField.getText(),apellidoField.getText(),comboAsig.getSelectedItem().toString()});
+            }
+        });
     }
 
     public static void main(String[] args) {
@@ -65,11 +71,10 @@ public class Boletin22 extends JFrame {
                                                        int row,
                                                        int column) {
             String nombreCelda = (String) table.getValueAt(row, 0); // Casteo a String y obtengo valores de la tabla para poder comparar
-
-            if (nombreCelda.equalsIgnoreCase("nombre")) {
+            if (nombreCelda.equalsIgnoreCase("nombre")) { // pongo el color gris de la fila si tiene como valor "nombre"
                 setBackground(Color.LIGHT_GRAY);
                 setForeground(Color.BLACK);
-                setHorizontalAlignment(SwingConstants.CENTER);
+                setHorizontalAlignment(SwingConstants.CENTER); // A las siguientes filas las pongo en blanco
             }else if (!nombreCelda.equalsIgnoreCase("nombre"))
                 setBackground(Color.WHITE);
             setForeground(Color.BLACK);
