e0c27469d510981f4ab7200fa60a618bd9ad40a8
Jose Ángel <jsamperevazquez@danielcastelao.org>
2021-05-09 15:57:05 +0200

    Boletin examen en proceso

---
+++ terceraEvaluacion/terceraEvaluacion/com/angel/extraExamen/Loteria.java
@@ -0,0 +1,70 @@
+package com.angel.extraExamen;
+
+import javax.swing.*;
+
+/**
+ * Creado por @autor: angel
+ * El  06 de may. de 2021.
+ * //-encoding utf8 -docencoding utf8 -charset utf8(Para el javadoc)
+ **/
+public class Loteria {
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton a7Button;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JButton a10Button;
+    private JButton a11Button;
+    private JButton a12Button;
+    private JButton a13Button;
+    private JButton a14Button;
+    private JButton a15Button;
+    private JButton a16Button;
+    private JButton a17Button;
+    private JButton a18Button;
+    private JButton a19Button;
+    private JButton a20Button;
+    private JButton a21Button;
+    private JButton a22Button;
+    private JButton a23Button;
+    private JButton a24Button;
+    private JButton a25Button;
+    private JButton a26Button;
+    private JButton a27Button;
+    private JButton a28Button;
+    private JButton a29Button;
+    private JButton a30Button;
+    private JButton a31Button;
+    private JButton a32Button;
+    private JButton a33Button;
+    private JButton a34Button;
+    private JButton a35Button;
+    private JButton a36Button;
+    private JButton a37Button;
+    private JButton a38Button;
+    private JButton a39Button;
+    private JButton a40Button;
+    private JButton a41Button;
+    private JButton a42Button;
+    private JButton a43Button;
+    private JButton a44Button;
+    private JButton a45Button;
+    private JButton a46Button;
+    private JButton a47Button;
+    private JButton a48Button;
+    private JButton a49Button;
+    private JPanel panel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Loteria");
+        frame.setContentPane(new Loteria().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
